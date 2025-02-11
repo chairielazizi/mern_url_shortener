d0ee5fab3fadf239a508339f0a9aed253bfb91fa
chairielazizi <azizihr2000@gmail.com>
2025-02-11 10:11:20 +0800

    fix the getall and create api request, implement the get url by id and increment the clicks, implement the delete request

--- server/controllers/ShortURLController.ts
+++ server/controllers/ShortURLController.ts
@@ -1,16 +1,16 @@
 import express, { Request, Response } from "express";
-import ShortURL from "../models/ShortURL";
+import ShortURLModel from "../models/ShortURL";
 
 export const createURL = async (req: Request, res: Response) => {
+  console.log("The original URL is: " + req.body.originalURL);
   const { originalURL } = req.body;
-  console.log("The original URL is: " + { originalURL });
 
   try {
-    const urlFound = await ShortURL.findOne({ originalURL });
+    const urlFound = await ShortURLModel.findOne({ originalURL });
     if (urlFound) {
       res.status(409).send({ urlFound });
     } else {
-      const shortURL = await ShortURL.create({ originalURL });
+      const shortURL = await ShortURLModel.create({ originalURL });
       res.status(201).send({ shortURL });
     }
   } catch (error) {
@@ -20,13 +20,10 @@ export const createURL = async (req: Request, res: Response) => {
 };
 
 export const getAllURL = async (req: Request, res: Response) => {
-  const shortURLs = await ShortURL.find();
   try {
+    const shortURLs = await ShortURLModel.find();
     if (shortURLs) {
-      res
-        .status(200)
-        // .json({ shortURLs })
-        .send({ message: "Short URLs not found!" });
+      res.status(200).send({ shortURLs });
     } else {
       res.status(404).send({ message: "No URLs found" });
     }
@@ -37,8 +34,32 @@ export const getAllURL = async (req: Request, res: Response) => {
 };
 
 export const getURLById = async (req: Request, res: Response) => {
-  const { originalURL } = req.body;
+  try {
+    const shortURL = await ShortURLModel.findOne({ shortURL: req.params.id });
+    if (shortURL) {
+      shortURL.clicks += 1;
+      await shortURL.save();
+      res.redirect(`${shortURL.originalURL}`);
+      //   res.status(200).send({ shortURL });
+    } else {
+      res.status(404).send({ message: "URL not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 };
+
 export const deleteURL = async (req: Request, res: Response) => {
-  const { originalURL } = req.body;
+  try {
+    const shortURL = await ShortURLModel.findByIdAndDelete({
+      _id: req.params.id,
+    });
+    if (shortURL) {
+      res.status(200).send({ message: "URL deleted successfully" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
 };
